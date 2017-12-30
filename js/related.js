9840c80e29c958c1f7952644d08338c5fdce4885
Michael Otis <mikelotis123@gmail.com>
2017-12-30 12:50:53 -0700

    deleted unnecessary commented code and changed fill opacity

--- js/related.js
+++ js/related.js
@@ -83,7 +83,7 @@
                                 d:              proj.pathFromGeojson,
                                 stroke:         "#6e6a6a",
                                 fill: function(d, i){ return colorScale(i);},
-                                "fill-opacity": 0.65,
+                                "fill-opacity": 0.55,
                                 "stroke-width": 1 / (proj.scale) * 0.7
                             })
                             .append("title")
@@ -112,118 +112,7 @@
             }//svgOverlay
             
         }//dataProcessor
-        // //Show tile only within City of Edmonton
-        // //used BoundaryCanvas Leaflet plugin
-        // d3.json("./data/Edmonton_Boundary.geojson", function(data){
-
-        //     //my custom objects, attributes: filePath(str) and colors(array)
-        //     //colors from http://colorbrewer2.org/#type=sequential&scheme=BuGn&n=3
-        //     var maps = [
-        //                 {id:       "ward-map", 
-        //                 filePath: "./data/Edmonton_wards.json",
-        //                 colors:   ['#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#f7f7f7','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061']
-        //                 }, 
-
-        //                 {id:       "neighbourhood-map", 
-        //                 filePath: "./data/Edmonton_neighbourhoods.json",
-        //                 colors:   ['#a50026','#d73027','#f46d43','#fdae61','#fee090','#ffffbf','#e0f3f8','#abd9e9','#74add1','#4575b4','#313695']
-        //                 }, 
-
-        //                 {id:       "bylaw-zoning-map",
-        //                 filePath: "./data/Edmonton_zoning.json",
-        //                 colors:   ['#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf','#e6f598','#abdda4','#66c2a5','#3288bd']
-        //                 }
-        //             ],
-        //         wardMap,
-        //         button;
-
-                
-        //     //add leaflet map to each object
-        //     //apply overlays to map
-        //     maps.forEach(function(d){
-        //             d.leaflet = makeMap(d.id);
-        //             svgOVerlay(d.filePath, d.id, d.colors);
-        //         });
-
-        //     //add button to first map
-        //     wardMap = maps[0].leaflet.map;
-        //     button  = L.control({position: "topleft"});
-        //         button.onAdd  = function(map){
-        //             this._div           = L.DomUtil.create("div", "mybutton");
-        //             this._div.innerHTML = "<p class='button-info'><strong><a href='index.html' "+
-        //                                 "title='Layered view for Edmonton Neighbourhoods, Wards, & Zoning Bylaw' "+
-        //                                 "id='label'>Layered View</a></strong></p>";
-        //             return this._div;
-        //         };
-        //     button.addTo(wardMap);
-            
-        //     function makeMap(id) {
-        //         var northEast = L.latLng(53.77225408641424, -113.25668334960938),
-        //             southWest = L.latLng(53.315287860180106, -113.72634887695312),
-        //             bounds    = L.latLngBounds(southWest, northEast),
-        //             map       = L.map(id, {center: [53.5444, -113.4909], zoom: 10, minZoom: 10, maxBounds: bounds} ),
-        //             //make tile within edmonton bounds
-        //             tiles     = L.TileLayer.boundaryCanvas('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
-        //             {
-        //                 attribution:     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-        //                 boundary:         data.geometries[0],
-        //                 trackAttribution: true
-        //             }).addTo(map);
-
-        //         return {map: map};
-        //     }
-
-        //     function svgOVerlay(filePath, id, colors) {
-
-        //         d3.json(filePath, function(json) {
-
-        //             //http://bl.ocks.org/xEviL/0c4f628645c6c21c8b3a(L.d3SvgOverlay Geojson example)
-        //             //I made custom json object
-        //             //make overlay
-        //             var features        = json.data.map(function(d){ return d["the_geom"]; }),
-        //                 search          = d3.map(maps, function(d) { return d.id; }),
-        //                 colorScale      = d3.scale.quantize()
-        //                                     .domain([0, json.data.length -1])
-        //                                     .range(colors),
-        //                 featuresOverlay = L.d3SvgOverlay(function(sel, proj){
-
-        //                     var upd =  sel.selectAll("path").data(features);
-        //                     upd.enter().append("path")
-        //                     .attr({
-        //                             d:              proj.pathFromGeojson,
-        //                             stroke:         "#6e6a6a",
-        //                             fill: function(d, i){ return colorScale(i);},
-        //                             "fill-opacity": 0.65,
-        //                             "stroke-width": 1 / (proj.scale) * 0.7
-        //                     })
-        //                     .append("title")
-        //                     .html(tipHtml);
-        //                 });
-
-        //             //adding overlay to map
-        //             featuresOverlay.addTo(search.get(id).leaflet.map);
-
-
-        //             function tipHtml(d, i) {
-                        
-        //                 var object = json.data[i];
-        //                 switch (id) {
-        //                     case "bylaw-zoning-map":
-        //                         return object["zoning"] + "</br>" + object["descript"];
-        //                         break;
-                        
-        //                     default:
-        //                     return object["name"] + "</br>" + parseFloat(object["area_km2"]).toFixed(3)+" "+"km&#94;"+"2";
-        //                         break;
-        //                 }
-        //             };//tip html
-                    
-        //         });//json
-
-        //     }//svgOverlay
-
-        // });//Edmonton Boundary
-
+ 
     }//main
 
     window.onload = main;
